bc67ffa9be4874490b54c996965ec50238b8622c
IGusakov <iliag354@gmail.com>
2022-12-29 13:22:59 +0300

    Коммит первой проектной работы.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,45 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+
+        int peopleNumber = getPeopleNumber();
+
+
+        ArrayList<Product> productList = new ArrayList<>();
+
+        Scanner scanner = new Scanner(System.in);
+        String command = "";
+        while(true){
+            productList.add(new Product());
+            System.out.println("Введите новый товар или наберите \"завершить\".");
+            command = scanner.nextLine();
+            if(command.equalsIgnoreCase("завершить")){
+                System.out.println("Список составлен");
+                break;
+            }
+        }
+        new Calculator(peopleNumber, productList);
+    }
+
+    public static int getPeopleNumber() {
+        Scanner scanner;
+        while (true) {
+            System.out.println("На сколько человек делим счёт?");
+            scanner = new Scanner(System.in);
+            if (!scanner.hasNextInt()) {
+                System.out.println("Так не выйдет! Это не цифра...");
+                continue;
+            }
+            int peopleNumber = scanner.nextInt();
+            if (peopleNumber <= 1) {
+                System.out.println("Должно быть как минимум 2.");
+            } else {
+                System.out.println("Счёт будет разделён на " + peopleNumber + " человек");
+                return peopleNumber;
+            }
+        }
     }
 }
